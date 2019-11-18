9f3301f54b313683912b68c9226c4eeb2952b5d1
Sam Wu <swu@nait.ca>
2019-11-18 15:15:25 -0700

    changed h2 to a file

--- dmit2015-demo-expenseapp/src/main/java/expense/config/ApplicationConfig.java
+++ dmit2015-demo-expenseapp/src/main/java/expense/config/ApplicationConfig.java
@@ -8,7 +8,7 @@ import javax.enterprise.context.ApplicationScoped;
 	@DataSourceDefinition(
 			name="java:app/datasources/dmit2015-demo-expenseapp/h2DS",
 			className="org.h2.jdbcx.JdbcDataSource",
-			url="jdbc:h2:mem:exepnsedb",
+			url="jdbc:h2:file:~/expensedb",
 			user="sa",
 			password="sa"),
 })
